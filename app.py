6e3fa3a608c673b40bbe07f1d6acef1e99d60e4e
Julien Berman <julien.d.berman@gmail.com>
2022-11-24 01:30:14 -0500

    final commit of the night

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 # FLASK INSTALLATION NOTES:
-# First make sure python and pip are installed. 
+# First make sure python and pip are installed.
 # Then run pip install flask.
 # Make sure python interpreter matches python version in bottom right.
 """
 Imports
 """
-from flask import Flask, render_template, redirect, request, session, flash
-from werkzeug.security import check_password_hash, generate_password_hash
+from flask import Flask, render_template, redirect, request
 
 # configure application
 app = Flask("__name__")
@@ -15,13 +14,14 @@ app = Flask("__name__")
 """
 Routes
 """
-# home page
+
+
 @app.route("/")
 def index():
 
     return render_template("index.html")
 
-# log page
+
 @app.route("/tips", methods=["GET", "POST"])
 def log():
     if request.method == "POST":
@@ -29,19 +29,18 @@ def log():
     else:
         return render_template("/templates/log.html")
 
-# odds calculator page
+
 @app.route("/odds", methods=["GET", "POST"])
 def odds():
-    
+
     return render_template("/templates/odds.html")
 
-# tips page
+
 @app.route("/tips", methods=["GET", "POST"])
 def tips():
-    
+
     return render_template("/templates/tips.html")
 
 
 if __name__ == "__main__":
     app.run()
-
