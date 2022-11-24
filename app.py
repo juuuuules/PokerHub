d0e383da9d8eb9710cb6c0f0959eda593644c6df
Julien Berman <julien.d.berman@gmail.com>
2022-11-24 00:57:17 -0500

    commit

--- app.py
+++ app.py
@@ -1,17 +1,46 @@
-# note: to install flask, first make sure python and pip are installed. Then run 
-# pip install flask
-from flask import Flask, render_template
+# FLASK INSTALLATION NOTES:
+# First make sure python and pip are installed. 
+# Then run pip install flask.
+# Make sure python interpreter matches python version in bottom right.
+"""
+Imports
+"""
+from flask import Flask, render_template, redirect, request, session, flash
+from werkzeug.security import check_password_hash, generate_password_hash
 
+# configure application
 app = Flask("__name__")
 
 
+"""
+Routes
+"""
+
+# home page
 @app.route("/")
 def index():
-    pass
 
-@app.route("/log.html")
+    return render_template("index.html")
+
+# log page
+@app.route("/tips", methods=["GET", "POST"])
 def log():
-    pass
+    if request.method == "POST":
+        return redirect("/log.html")
+    else:
+        return render_template("/templates/log.html")
+
+# odds calculator page
+@app.route("/odds", methods=["GET", "POST"])
+def odds():
+    
+    return render_template("/templates/odds.html")
+
+# tips page
+@app.route("/tips", methods=["GET", "POST"])
+def tips():
+    
+    return render_template("/templates/tips.html")
 
 
 if __name__ == "__main__":
