c23ead5c7e801e0339bbb4e9e0c86730c7c73581
Julien Berman <julien.d.berman@gmail.com>
2022-11-27 20:22:33 -0500

    commit

---
+++ test.sql
@@ -0,0 +1,7 @@
+-- DROP TABLE users;
+
+-- CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT NOT NULL, hash TEXT NOT NULL);
+
+-- INSERT INTO users (email, hash) VALUES ('admin@example.com', 25);
+
+SELECT * FROM users
